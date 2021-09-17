8f38badca289bfd95789190dc8f27534c608d3f5
Mauricio Aguero <mam.121002@gmail.com>
2021-09-17 15:53:07 -0400

    IDs

--- schemaCaso3.ts
+++ schemaCaso3.ts
@@ -4,23 +4,24 @@ mongoose.connect('mongodb://localhost:27017/caso3');
 
 const articulos = mongoose.model('articulos', 
 {
-    owner: String,   //nombre del usuario que agrega el articulo a la subasta
+    articleID: Number,      //ID para identificar el articulo
+    owner: String,          //nombre del usuario que agrega el articulo a la subasta
     ownerEmail: String,
     publicationDate: Date,  //fecha en la que se pone el articulo en subasta
-    articleYear: Number,   //para indicar la antiguedad del articulo
+    articleYear: Number,    //para indicar la antiguedad del articulo
     articleName: String,
     description: String,
     pictureURL: String,
     initialPrice: Number,
     actualPrice: Number,
-    endDate: Date,  //fecha en la que termina la subasta del articulo
+    endDate: Date,          //fecha en la que termina la subasta del articulo
     active: Boolean
 })
 
 const bids = mongoose.model('articulos',
 {
-    
-    bidder: String,  //nombre del ofertador
-    amount: Number   //cantidad que oferta en la subasta
+    articleID: Number,      //para identificar cual articulo es
+    bidder: String,         //nombre del ofertador
+    amount: Number          //cantidad que oferta en la subasta
 
 })
